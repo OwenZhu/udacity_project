99ffa21589fb26a916db77ce987c70784e1d89b2
OwenZhu <398094982@qq.com>
2018-03-17 10:17:05 +1100

    update proj4

--- smartcab-master/smartcab/agent.py
+++ smartcab-master/smartcab/agent.py
@@ -40,7 +40,9 @@ class LearningAgent(Agent):
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
         
-        self.epsilon = self.epsilon * 0.992
+        # self.epsilon = self.epsilon * 0.992
+
+        self.epsilon -= 0.05
 
         if testing:
             self.epsilon = self.alpha = 0
@@ -193,14 +195,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, display=False, log_metrics=True, optimized=True)
+    sim = Simulator(env, update_delay=0.01, display=False, log_metrics=True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(tolerance=0.0, n_test=10)
 
 
 if __name__ == '__main__':
